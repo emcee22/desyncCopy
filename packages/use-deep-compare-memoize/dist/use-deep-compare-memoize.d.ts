9beb1fd5612f444b1545ca3b5b93f6f577b80880
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-02-15 17:34:28 +0100

    chore: add clean dist scripts

--- packages/use-deep-compare-memoize/dist/use-deep-compare-memoize.d.ts
+++
@@ -1,3 +0,0 @@
-declare type AcceptedValue = string | boolean | object | null;
-export declare function useDeepCompareMemoize(value: AcceptedValue): AcceptedValue;
-export {};
--- packages/use-promise/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./use-promise.cjs.production.min.js');
-} else {
-  module.exports = require('./use-promise.cjs.development.js');
-}
