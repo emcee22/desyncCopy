31f754f21260e78cbdb00bf5b79d077cc9704923
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-03-23 16:37:21 +0100

    tmp

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -3,7 +3,7 @@ import { usePromise, useDeepCompareMemoize, Result } from '../src';
 describe('@desync/core', () => {
   it('should correctly import the Result package', async () => {
     expect(
-      Result.ok<unknown>({ username: 'Jest' }).isError()
+      Result.ok<unknown>({ username: 'Name' }).isError()
     ).toBeFalsy();
   });
 });
