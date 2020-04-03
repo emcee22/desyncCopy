0d29b312a0c0558fa836a9dcb890f0b4cac9988a
Paul Harbuz <paulharbuz@gmail.com>
2020-04-03 18:41:17 +0200

    chore: trying to check the version update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@desync/use-deep-compare-memoize';
 export * from '@desync/use-promise';
 console.log('asfjgsaghdfsaghdghsa');
+console.log('asfjgsaghdfsaghdghsa');
 export * from '@desync/result';
